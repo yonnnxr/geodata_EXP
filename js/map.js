e91b3d85daf60bf3800bb1d2aa314589274907db
Carlos Eduardo <carlosborges2007edu@gmail.com>
2025-05-15 21:20:27 -0400

    teste

--- js/map.js
+++ js/map.js
@@ -1,34 +1,19 @@
 const map = L.map('map').setView([-20.4695, -54.6052], 13);
 
 const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap',
-    maxZoom: 19
+  attribution: '© OpenStreetMap',
+  maxZoom: 19
 }).addTo(map);
 
-const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/' +
-    'World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '© Esri',
-    maxZoom: 19
+const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: '© Esri',
+  maxZoom: 19
 });
 
 L.control.layers({
-    "Mapa": osmLayer,
-    "Satélite": satelliteLayer
+  "Mapa": osmLayer,
+  "Satélite": satelliteLayer
 }).addTo(map);
-
-const sidebar = document.getElementById('sidebar');
-const menuToggle = document.getElementById('menu-toggle');
-const closeBtn = document.getElementById('close-btn');
-
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.add('open');
-    menuToggle.style.display = 'none';
-});
-
-closeBtn.addEventListener('click', () => {
-    sidebar.classList.remove('open');
-    menuToggle.style.display = 'block';
-});
 
 let streetViewMode = false;
 let pegmanMarker = null;
@@ -36,49 +21,48 @@ let pegmanMarker = null;
 const streetViewControl = L.control({ position: 'topleft' });
 
 streetViewControl.onAdd = function () {
-    const div = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
-    div.innerHTML = `
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Street_View_icon.svg/32px-Street_View_icon.svg.png"
-             alt="Street View"
-             style="width: 26px; height: 26px; cursor: pointer;"
-             title="Selecionar local para Street View">`;
+  const div = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+  div.innerHTML = `
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Street_View_icon.svg/32px-Street_View_icon.svg.png"
+         alt="Street View"
+         style="width: 26px; height: 26px; cursor: pointer;"
+         title="Clique para escolher o local">`;
 
-    div.onclick = function () {
-        streetViewMode = true;
-        alert("Clique no mapa para abrir o Street View.");
-        if (pegmanMarker) {
-            map.removeLayer(pegmanMarker);
-            pegmanMarker = null;
-        }
-    };
+  div.onclick = function () {
+    streetViewMode = true;
+    map._container.style.cursor = 'crosshair';
+    alert("Clique no mapa onde deseja abrir o Street View.");
+  };
 
-    return div;
+  return div;
 };
 
 streetViewControl.addTo(map);
 
 map.on('click', function (e) {
-    if (!streetViewMode) return;
+  if (!streetViewMode) return;
 
-    const lat = e.latlng.lat;
-    const lng = e.latlng.lng;
+  const lat = e.latlng.lat;
+  const lng = e.latlng.lng;
 
-    const pegmanIcon = L.icon({
-        iconUrl: 'https://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-        popupAnchor: [0, -32]
-    });
+  if (pegmanMarker) {
+    map.removeLayer(pegmanMarker);
+  }
 
-    if (pegmanMarker) {
-        map.removeLayer(pegmanMarker);
-    }
+  const pegmanIcon = L.icon({
+    iconUrl: 'https://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  });
 
-    pegmanMarker = L.marker([lat, lng], { icon: pegmanIcon }).addTo(map)
-        .bindPopup("Abrindo Street View...")
-        .openPopup();
-    const url = `https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=${lat},${lng}`;
-    window.open(url, '_blank');
+  pegmanMarker = L.marker([lat, lng], { icon: pegmanIcon }).addTo(map)
+    .bindPopup("Abrindo Street View...")
+    .openPopup();
 
-    streetViewMode = false;
+  const url = `https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=${lat},${lng}`;
+  window.open(url, '_blank');
+
+  streetViewMode = false;
+  map._container.style.cursor = '';
 });
